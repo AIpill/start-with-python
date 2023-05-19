ac243bcb6b20d865c37b2f40a942ee1d6315d9a3
AIpills <x9one@pop-os.localdomain>
2023-05-19 18:44:15 +0100

    Replace spaces with 3 dots

---
+++ Functions and Variables/01-playback.py
@@ -0,0 +1,9 @@
+# Ask for user Input 
+userInput = input() # Default return string
+
+# Output user input by replacing space with 3 dots
+print(userInput.replace(" ","...")) # Using string method replace()
+
+# Test 
+    # Input -> 'You are welcome', Output -> 'You...are...welcome'
+    # Input -> 'My journey', Output -> 'My...journey' 
