7fbc02032e0a6d0c891491395808adaaf61c324a
AIpills <x9one@pop-os.localdomain>
2023-05-19 18:35:57 +0100

    Test use cases for indoor problem

--- Functions and Variables/indoor.py
+++ Functions and Variables/indoor.py
@@ -3,3 +3,7 @@ userInput = input() # By default return string
 
 # Output user input in lowercase
 print(userInput.lower()) # Use string method lower()
+
+# Test 
+# Write 'HELLO, WORLD' must print 'hello, world'
+# Write "PYTHON", must print 'python'
